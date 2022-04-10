db1469a959f320177f108b4efa3cefd216309272
onflou <onflou@onflouui-MacBookPro.local>
2022-04-11 01:27:33 +0900

    add check nickname

--- routers/user.js
+++ routers/user.js
@@ -24,4 +24,36 @@ router.get('/me', auth.isSignIn, async function(req, res) {
     }
 });
 
+
+router.get('/check/nickName/:nickName', auth.isSignIn, async function(req, res) {
+
+    try {
+
+        var result = await ModelUser.findOne({
+            nickName: req.params.nickName,
+        })
+    
+        if(result == null) {
+            res.json(response.success({result: 1, message: '사용할 수 있습니다.'}));
+        } else {
+            res.json(response.success({result: 0, message: '이미 사중입니다.'}));
+        }
+
+    } catch (err) {
+        var error = convertException(err)
+        res.json(response.fail(error, error.errmsg, error.code))
+    }
+});
+
+router.patch("/", auth.isSignIn, (req, res) => {
+    ModelUser.findByIdAndUpdate({_id: req.decoded.id}, {$set: req.body})
+    .exec()
+    .then((_) => res.json(response.success(_)))
+    .catch((_) => {
+        var error = convertException(_)
+        res.json(response.fail(error, error.errmsg, error.code))
+    });
+});
+
+
 module.exports = router;
